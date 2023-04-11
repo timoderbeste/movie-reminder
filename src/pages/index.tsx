480b2ff8369e4d1efdff10d2c4b654a20450359e
Timo Wang <ntwang1994@gmail.com>
2023-04-10 19:34:43 -0500

    Splitting the SPA into two routes: one for searching and one for viewing bookmarks.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
-import Head from 'next/head'
-import App from "./_app"
+import Head from "next/head";
+import App from "./_app";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Movie Reminder</title>
-        <meta name="description" content="Reminding people of movies to watch" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Reminding people of movies to watch"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
       </Head>
-      <main>
-        <App />
-      </main>
+      <main></main>
     </>
-  )
+  );
 }
